1871102cf13df073f4c4b1b937e74fe876f3df35
cwh116 <ittechhunter513@gmail.com>
2022-07-08 20:35:48 +0800

    QR-Code

---
+++ node/web/frontend/App.jsx
@@ -0,0 +1,26 @@
+import Routes from "./Routes";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  AppBridgeProvider,
+  GraphQLProvider,
+  PolarisProvider,
+} from "./components";
+
+export default function App() {
+  // Any .tsx or .jsx files in /pages will become a route
+  // See documentation for <Routes /> for more info
+  const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
+
+  return (
+    <PolarisProvider>
+      <BrowserRouter>
+        <AppBridgeProvider>
+          <GraphQLProvider>
+            <Routes pages={pages} />
+          </GraphQLProvider>
+        </AppBridgeProvider>
+      </BrowserRouter>
+    </PolarisProvider>
+  );
+}
---
+++ node/web/middleware/auth.js
@@ -0,0 +1,91 @@
+import { Shopify } from "@shopify/shopify-api";
+import { gdprTopics } from "@shopify/shopify-api/dist/webhooks/registry.js";
+
+import topLevelAuthRedirect from "../helpers/top-level-auth-redirect.js";
+
+export default function applyAuthMiddleware(app) {
+  app.get("/api/auth", async (req, res) => {
+    if (!req.signedCookies[app.get("top-level-oauth-cookie")]) {
+      return res.redirect(`/api/auth/toplevel?shop=${req.query.shop}`);
+    }
+
+    const redirectUrl = await Shopify.Auth.beginAuth(
+      req,
+      res,
+      req.query.shop,
+      "/api/auth/callback",
+      app.get("use-online-tokens")
+    );
+
+    res.redirect(redirectUrl);
+  });
+
+  app.get("/api/auth/toplevel", (req, res) => {
+    res.cookie(app.get("top-level-oauth-cookie"), "1", {
+      signed: true,
+      httpOnly: true,
+      sameSite: "strict",
+    });
+
+    res.set("Content-Type", "text/html");
+
+    res.send(
+      topLevelAuthRedirect({
+        apiKey: Shopify.Context.API_KEY,
+        hostName: Shopify.Context.HOST_NAME,
+        shop: req.query.shop,
+      })
+    );
+  });
+
+  app.get("/api/auth/callback", async (req, res) => {
+    try {
+      const session = await Shopify.Auth.validateAuthCallback(
+        req,
+        res,
+        req.query
+      );
+
+      const host = req.query.host;
+      app.set(
+        "active-shopify-shops",
+        Object.assign(app.get("active-shopify-shops"), {
+          [session.shop]: session.scope,
+        })
+      );
+
+      const responses = await Shopify.Webhooks.Registry.registerAll({
+        shop: session.shop,
+        accessToken: session.accessToken,
+      });
+
+      Object.entries(responses).map(([topic, response]) => {
+        if (!response.success && !gdprTopics.includes(topic)) {
+          console.log(
+            `Failed to register ${topic} webhook: ${response.result.errors[0].message}`
+          );
+        }
+      });
+
+      // Redirect to app with shop parameter upon auth
+      res.redirect(`/?shop=${session.shop}&host=${host}`);
+    } catch (e) {
+      console.warn(e);
+      switch (true) {
+        case e instanceof Shopify.Errors.InvalidOAuthError:
+          res.status(400);
+          res.send(e.message);
+          break;
+        case e instanceof Shopify.Errors.CookieNotFound:
+        case e instanceof Shopify.Errors.SessionNotFound:
+          // This is likely because the OAuth session cookie expired before the merchant approved the request
+          res.redirect(`/api/auth?shop=${req.query.shop}`);
+          break;
+        default:
+          res.status(500);
+          res.send(e.message);
+          break;
+      }
+    }
+  });
+}
